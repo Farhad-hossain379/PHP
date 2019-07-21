5707206045ec74aab4edb30d14f0b67bf6b40d2b
Farhad-hossain379 <52917870+Farhad-hossain379@users.noreply.github.com>
2019-07-21 17:21:32 +0600

    Add files via upload

---
+++ SeipPhp71/day-16/array.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/*$ary=[];
+$ary[0]=10;
+$ary[1]=20;
+$ary[2]=30;
+$ary[3]=40;
+
+$ary['name ']="Farhad";
+$ary['City']="Dhaka";
+$ary['Mobile']="01780732379";
+
+//print $ary[1];
+
+foreach ($ary as $value){
+    print $value.'<br/>';
+    */
+
+/*$ary = array(
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+);
+
+for ($row = 0; $row < 4; $row++) {
+    echo "Row number $row";
+
+        for ($col = 0; $col < 3; $col++) {
+            echo "<li>".$ary[$row][$col]."</li>";
+        }
+
+}*/
+
---
+++ SeipPhp71/day15/Syntax.php
@@ -0,0 +1,34 @@
+<!--<html>
+    <head>
+        <body>
+            <h1 > <?php /*echo "heading h1"*/?></h1>
+            <input type="text" value="<?php /*echo "Enter text";*/?>"/>
+        </body>
+    </head>
+</html>-->
+
+
+<?php
+
+ /*$firstName = "Farhad";
+ $lastName = "Hossain";
+ print $firstName.$lastName;
+ //print $lastName;*/
+
+
+/*$firstName = "Farhad";
+$lastName = "Hossain";
+
+echo $firstName;
+echo '<br/>';
+echo $lastName;*/
+
+function functionName(){
+    echo"Function";
+}
+functionName();
+
+
+
+
+
