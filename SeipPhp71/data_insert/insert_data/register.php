933f7ca8bf16c75f4ea65c61408ff699168fed4b
Farhad-hossain379 <52917870+Farhad-hossain379@users.noreply.github.com>
2019-07-27 07:59:32 +0600

    Add files via upload

--- SeipPhp71/data_insert/insert_data/register.php
+++ SeipPhp71/data_insert/insert_data/register.php
@@ -1,10 +1,9 @@
 <?php
-
-$id_value = $_POST['id'];
+//
+//$id_value = $_POST['id'];
 $name_value = $_POST['name'];
 $email_value = $_POST['Email'];
 $phone_value = $_POST['phone'];
-
 
 $connection=mysqli_connect("localhost","root","","php_db");
 
@@ -12,17 +11,30 @@ if(!$connection){
     die("Error in database connection");
 }
 
-$result = mysqli_query($connection,"insert into register values('$id_value','$name_value','$email_value', '$phone_value')");
+$query="insert into register (`Name`,`Email`,`phone`) values('$name_value','$email_value', '$phone_value')";
+$result = mysqli_query($connection,$query);
 // query value er modde obossoi id er variable dite hobe
+
+
 
 if(!$result){
     die("Error in query");
 }
-else{
-    echo "Data inserted successfully";
+/*else{
+
+    echo "Data inserted successfully" ;
+
+}*/
+else
+{
+
+    echo '<script language="javascript">';
+    echo 'alert("Successfully Registered"); location.href="data_insert_form.php"';
+    echo '</script>';
 }
 
-header("Location: Form.html");
+
+
 
 
 
--- SeipPhp71/data_insert/update2/update_index.php
+++ SeipPhp71/data_insert/update2/update_index.php
@@ -1,22 +1,33 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "php_db";
+if (isset($_POST['btn'])){
+    include_once ('update_form.html');
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "php_db";
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "UPDATE register SET Name = '".$name."',Email = '".$email."',phone = '".$phone."' WHERE register.ID = $id ";
+
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+
+    $conn->close();
 }
 
-$sql = "UPDATE register SET Name = 'Morium' WHERE ID = 2";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();
